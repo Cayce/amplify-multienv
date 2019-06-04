d0199a26fbc533cd0af265536b4532c9b597064c
Kirill Chudinov <a@Kirills-MacBook-Pro.local>
2019-06-04 14:47:44 +0300

    v

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import * as queries from "./graphql/queries";
 import * as subscriptions from "./graphql/subscriptions";
 
 Amplify.configure(awsConfig);
+PubSub.configure();
 
 let username = 'Kirill'
 let password = 'Xdr%4321'
@@ -23,17 +24,28 @@ Auth.signIn(username, password)
 
 Auth.currentCredentials().then((info) => {
     const cognitoIdentityId = info._identityId;
+    debugger;
     console.log(cognitoIdentityId);
 });
 
 Amplify.addPluggable(new AWSIoTProvider({
     aws_pubsub_region: 'us-east-2',
-    aws_pubsub_endpoint: 'wss://a73c5uwwia0zy-ats.iot.us-east-2.amazonaws.com'
+    aws_pubsub_endpoint: 'a73c5uwwia0zy-ats.iot.us-east-2.amazonaws.com'
 }))
 
 Amplify.addPluggable(new MqttOverWSProvider({
     aws_pubsub_endpoint: 'wss://iot.eclipse.org:443/mqtt'
 }))
+
+let sub = {
+    next: data => console.log('Message received', data),
+    error: error => console.error(error),
+    close: () => console.log('Done')
+}
+PubSub.subscribe('silo-mobile-app').subscribe(sub)
+
+console.log('Message sent')
+PubSub.publish('silo-mobile-app', { msg: 'Hello to all subscribers!' })
 
 const ListView = ({ todos }) => (
   <div>
